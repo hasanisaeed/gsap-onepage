4681b7bac854fc3e4dd7b36f952df8128dc761da
Saeed <hassanisaeed19@yahoo.com>
2021-10-10 17:11:24 +0330

    fix: remove extra files

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'homepage',
+    title: 'gsap one page',
     htmlAttrs: {
       lang: 'en'
     },
