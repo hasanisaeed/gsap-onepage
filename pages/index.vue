886aa684d3598c23b40dfbd48d92436ba2157c5b
Saeed <hassanisaeed19@yahoo.com>
2021-10-12 22:45:19 +0330

    fix: update timeline

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,14 @@
 <template>
-  <div >
+  <div>
     <div class="intro">
-      <h4  class="intro__text">
-        در شهریور 96، جمعی از نخبگان کارآفرینی، فناوری و لجستیک گرد هم آمدند و برای حل چالش های حمل و نقل جاده ای کالای کشور، ایده دانش بنیان سامانه هوشمند حمل بار را در قامت باروان پدید آوردند تا صاحبان کالا دغدغه حمل نداشته، رانندگان کامیون همواره به بار دسترسی داشته و بهره وری و خدمات شرکت های حمل و نقلی توسعه پیدا کند.
+      <h4 class="intro__text">
+        در شهریور 96، جمعی از نخبگان کارآفرینی، فناوری و لجستیک گرد هم آمدند و برای حل چالش های حمل و نقل جاده ای کالای
+        کشور، ایده دانش بنیان سامانه هوشمند حمل بار را در قامت باروان پدید آوردند تا صاحبان کالا دغدغه حمل نداشته،
+        رانندگان کامیون همواره به بار دسترسی داشته و بهره وری و خدمات شرکت های حمل و نقلی توسعه پیدا کند.
 
-        خاطر آسوده صاحب کالا، لبخند رضایت راننده و اعتماد شرکت حمل و نقلی ارزش های ماست. می کوشیم با تحقیق و توسعه در کنار تجربیات سازنده کیفیت خدمات باروان را بالا برده و چشم اندازی روشن و با ارزش برای صنعت حمل و نقل کالای کشور بسازیم و آن را در تراز جهانی ارائه کنیم.
+        خاطر آسوده صاحب کالا، لبخند رضایت راننده و اعتماد شرکت حمل و نقلی ارزش های ماست. می کوشیم با تحقیق و توسعه در
+        کنار تجربیات سازنده کیفیت خدمات باروان را بالا برده و چشم اندازی روشن و با ارزش برای صنعت حمل و نقل کالای کشور
+        بسازیم و آن را در تراز جهانی ارائه کنیم.
 
       </h4>
       <h4 class="intro__text-ios">کاروان</h4>
@@ -16,7 +20,8 @@
       </div>
       <div class="text">
         <h1>
-          رانندگان عزیز با پیوستن به سامانه هوشمند باروان، به بزرگترین سالن اعلام بار مجازی کشور دسترسی پیدا کنید، هزینه ها را کاهش و درآمد خود را بالا ببرید:
+          رانندگان عزیز با پیوستن به سامانه هوشمند باروان، به بزرگترین سالن اعلام بار مجازی کشور دسترسی پیدا کنید، هزینه
+          ها را کاهش و درآمد خود را بالا ببرید:
         </h1>
 
         <h1>
@@ -26,33 +31,32 @@
         <h1>
           باروان چیست؟
           سامانه هوشمند حمل بار
-          باروان یک سامانه اعلام بار و حمل بار است. در این سامانه صاحب کالا به راحتی اعلام بار کرده و بار خود را در معرض دید و انتخاب هزاران راننده کامیون از سراسر کشور قرار می دهد
+          باروان یک سامانه اعلام بار و حمل بار است. در این سامانه صاحب کالا به راحتی اعلام بار کرده و بار خود را در معرض
+          دید و انتخاب هزاران راننده کامیون از سراسر کشور قرار می دهد
         </h1>
 
         <h1 class="text__last">اپلیکیشن رانندگان باروان را از اینجا دانلـود و به سادگی ثبت نام کنید!</h1>
       </div>
     </div>
 
-<div class="team">
-
-  <div class="on" style="background: green">
-    TEST
-    <h1 style="color: black;">    TEST   TEST   TEST   TEST   TEST   TEST   TEST   TEST  TEST</h1>
-  </div>
-  <div class="on" style="background: red">
-    TEST
-    <h1 style="color: black;">    TEST   TEST   TEST   TEST   TEST   TEST   TEST   TEST  TEST</h1>
-  </div>
-  <div class="on" style="background: yellowgreen">
-    TEST
-    <h1 style="color: black;">    TEST   TEST   TEST   TEST   TEST   TEST   TEST   TEST  TEST</h1>
-  </div>
-  <div class="on" style="background: darkmagenta">
-    TEST
-    <h1 style="color: black;">    TEST   TEST   TEST   TEST   TEST   TEST   TEST   TEST  TEST</h1>
-  </div>
-
-</div>
+    <div class="team">
+      <div class="on" style="background: green;height:150px;">
+        TEST
+        <h1 style="color: black;"> TEST TEST TEST TEST TEST TEST TEST TEST TEST</h1>
+      </div>
+      <div class="on" style="background: red;height:150px;">
+        TEST
+        <h1 style="color: black;"> TEST TEST TEST TEST TEST TEST TEST TEST TEST</h1>
+      </div>
+      <div class="on" style="background: yellowgreen;height:150px;">
+        TEST
+        <h1 style="color: black;"> TEST TEST TEST TEST TEST TEST TEST TEST TEST</h1>
+      </div>
+      <div class="on" style="background: darkmagenta;height:150px;">
+        TEST
+        <h1 style="color: black;"> TEST TEST TEST TEST TEST TEST TEST TEST TEST</h1>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -99,7 +103,7 @@ export default {
         scrollTrigger: {
           trigger: ".phone",
           start: "center center",
-          end: "center 60%",
+          // end: "center 60%",
           endTrigger: ".text__last",
           pinSpacing: true,
           pin: true
@@ -110,8 +114,8 @@ export default {
           opacity: 1,
           scrollTrigger: {
             trigger: title,
-            start: "top 55%",
-            end: "top 10%",
+            // start: "top 55%",
+            // end: "top 10%",
             scrub: true,
             toggleClass: {
               targets: ".phone",
@@ -129,8 +133,8 @@ export default {
         }
       });
 
-      const boxEls =  document.querySelectorAll("on") ;
-      tl2.from(boxEls, {
+      const boxEls = document.querySelectorAll("on");
+      tl2.to(boxEls, {
         opacity: 0,
         marginLeft: 0,
         transform: "scale(1.5)",
